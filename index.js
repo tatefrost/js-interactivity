796038c4baeae87f50db831932bcc48ee3e6cfd6
Tate Frost <tatefrost96@gmail.com>
2021-11-23 16:26:47 -0700

    did intermidiate

--- index.js
+++ index.js
@@ -22,15 +22,23 @@ document.querySelector('form').addEventListener('submit', addMovie)
 
 const deleteMovie = event => {
   event.target.parentNode.remove()
-  message.textContent = 'Movie deleted'
-
+  message.textContent = `${event.target.parentNode.firstChild.textContent} deleted`
+  revealMessage()
 }
 
 const crossOffMovie = event => {
     event.target.classList.toggle('checked')
     if (event.target.classList.contains('checked')){
-        message.textContent = 'Movie watched'
+        message.textContent = `${event.target.textContent} watched`
     } else {
-        message.textContent = 'Movie added back'
+        message.textContent = `${event.target.textContent} added back`
     }
+    revealMessage()
 }
+
+const revealMessage = () => {
+  message.classList.remove('hide')
+  setTimeout(() => {
+    message.classList.add('hide')
+  }, 3000)
+  }
